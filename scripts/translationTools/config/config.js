5a8c632c69c595f3997ee7013a2f46cfe3bf9a02
alexdm0 <alexdemulder@gmail.com>
2019-07-11 13:49:26 +0200

    initial commit

---
+++ scripts/translationTools/config/config.js
@@ -0,0 +1,21 @@
+const FILE_EXCLUSIONS = {
+  'DebugIconSelection': true,
+  'IconDebug': true,
+  'styles':true,
+};
+
+const FILE_KEY_EXCEPTIONS = {
+  'AppUtil': true,
+  'LocationHandler': true,
+  'StoneUtil': true,
+  'Tabs': true,
+  '__UNIVERSAL': true
+};
+
+module.exports = {
+  FILE_EXCLUSIONS,
+  FILE_KEY_EXCEPTIONS,
+  ENGLISH_BASE_LANGUAGE_PATH: "../../js/localization/en/us/en_us.ts",
+  BASE_CODE_PATH:             "../../js",
+  LOCALIZATION_BASE_PATH:     "../../js/localization",
+}
---
+++ scripts/translationTools/lib/lineKeyChecker.js
@@ -0,0 +1,102 @@
+let fs = require( 'fs' );
+let path = require( 'path' );
+let util = require("./util");
+let config = require("../config/config");
+let extractor = require("./extractor");
+
+const LANGUAGE_FILE_PATH = '../../js/'
+
+let translationData = {};
+
+let fileList = [];
+let fileMap = {}
+
+let parseFilesRecursivelyInPath = function(dirPath, BASE) {
+  let files = fs.readdirSync( dirPath )
+
+  for (let i = 0; i < files.length; i++) {
+    let file = files[i];
+
+    // Make one pass and make the file complete
+    let elementPath = path.join( dirPath, file );
+    let stat = fs.statSync(elementPath)
+    let ext = elementPath.substr(elementPath.length - 3);
+
+    if (stat.isFile() && (ext === "tsx")) {
+      fileList.push(elementPath)
+      parseFile(elementPath, false, BASE);
+    }
+    else if (stat.isDirectory()) {
+      // console.log( "'%s' is a directory.", elementPath );
+      parseFilesRecursivelyInPath(elementPath, BASE)
+    }
+  };
+
+  return {fileMap, fileList, translationData}
+}
+
+
+let parseFile = function(filePath, allowReplace, BASE) {
+  let content    = fs.readFileSync(filePath, "utf8")
+  let proposedFilenameRegex  = /Languages\.get\("(.*)"/gm
+  let labelRegex  = /lang\(['"](.*?)['"].*?\)/gm
+
+  let labelMatches = content.match(labelRegex);
+  let filenameMatches = content.match(proposedFilenameRegex);
+
+  let filenameArr = filePath.split("/");
+  let filename = filenameArr[filenameArr.length-1].replace(".tsx","").replace(/[^0-9a-zA-Z]/g,'_');
+
+  if (config.FILE_EXCLUSIONS[filename]) { return }
+
+  let filenameForTranslation = null;
+
+  if (filenameMatches !== null) {
+    for ( let i = 0; i < filenameMatches.length; i++) {
+      let match = filenameMatches[i];
+      let resultArray = [];
+      while ((resultArray = proposedFilenameRegex.exec(match)) !== null) {
+        filenameForTranslation = resultArray[1];
+        break;
+      }
+    }
+  }
+
+  if (labelMatches !== null) {
+    if (filenameForTranslation !== filename) {
+      console.log("Warning: file", filename, " is not using it's own filename as translation key but:", filenameForTranslation)
+    }
+
+    if (!filenameForTranslation) {
+      console.log("ERR: NO TRANSLATION FILE IS REFERENCED!", filename)
+      return;
+    }
+
+    for ( let i = 0; i < labelMatches.length; i++) {
+      let match = labelMatches[i];
+      let resultArray = [];
+      while ((resultArray = labelRegex.exec(match)) !== null) {
+        let key = resultArray[1];
+        if (BASE[filenameForTranslation] === undefined) {
+          if (BASE['__UNIVERSAL'][key] === undefined) {
+            console.log("MISSING FILE ENTRY", filename, key)
+          }
+        }
+        else {
+          if (BASE[filenameForTranslation][key] === undefined) {
+            if (BASE['__UNIVERSAL'][key] === undefined) {
+              console.log("MISSING KEY", key, "in", filename)
+            }
+          }
+        }
+      }
+    }
+  }
+}
+
+function checkLineElements(dirPath, BASE) {
+  return parseFilesRecursivelyInPath(dirPath, BASE)
+}
+
+
+module.exports = {checkLineElements}
